b02a092620a3baff2ef154820147b9a1685ab582
Andrei Dodu <andrei.dodu@hotmail.it>
2022-08-07 12:50:02 +0200

    removed useless import

--- src/main/java/com/andreidodu/qm/commands/group/CommandGroupUpdate.java
+++ src/main/java/com/andreidodu/qm/commands/group/CommandGroupUpdate.java
@@ -1,18 +1,13 @@
 package com.andreidodu.qm.commands.group;
 
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.andreidodu.qm.commands.Command;
 import com.andreidodu.qm.commands.common.category.CommandUpdateCategoryCommon;
-import com.andreidodu.qm.constants.ConsoleConstants;
 import com.andreidodu.qm.dto.Group;
-import com.andreidodu.qm.dto.input.GroupInsert;
 import com.andreidodu.qm.service.category.GroupService;
 import com.andreidodu.qm.service.common.CategoryCommonService;
-import com.andreidodu.qm.util.ConsoleUtil;
 
 @Component
 public class CommandGroupUpdate extends CommandUpdateCategoryCommon<Group> implements Command {
